84befeef1922bbd74fbd93c5890e4bbc5bc74261
MayconTi01 <maycontiestudos01@gmail.com>
2026-06-06 09:46:53 -0300

    Aula: Request 2
    Mod2-Servidor Simples

--- client.mjs
+++ client.mjs
@@ -1,6 +1,11 @@
 const response = await fetch ('http://localhost:3000/produtos?cor=verde&tamanho=g',{
-     method:'POST', 
-    headers: { 'content-type': 'application/json'}} ); 
+    method:'POST', 
+    headers: { 'content-type': 'application/json'}, 
+    body : JSON.stringify({username: 'Maycon', senha:"12345"})
+
+
+
+} ); 
 
 
 const body = await response.text();  
--- server.mjs
+++ server.mjs
@@ -1,6 +1,28 @@
 import { createServer } from "node:http"; 
 
-const server = createServer((req,res) => { 
+const frase1  =  Promise.resolve( 'Ola'); 
+const frase2 = Promise.resolve('Mundo'); 
+const frasesPromises = [ frase1, frase2]; 
+const frases = [];
+
+for await ( const frase of frasesPromises) { 
+    console.log(frase); 
+    frases.push(frase); 
+}
+//console.log(frases.join()); 
+
+const parte1 = Buffer.from('Ola '); 
+const parte2 = Buffer.from( 'Mundo'); 
+const final = Buffer.concat([parte1,parte2]); 
+//console.log(final);  
+//console.log(final.toString('utf-8')); 
+// console.log(parte1)
+
+
+
+
+
+const server = createServer( async(req,res) => { 
     res.statusCode = 200; 
     res.setHeader("content-type","text/html")
 
@@ -8,12 +30,23 @@ const url = new URL (req.url, 'http://localhost');
 
 const cor = url.searchParams.get('cor'); 
 const tamanho = url.searchParams.get('tamanho');  
-console.log(req.headers['content-type'])
-console.log(req.rawHeaders)
-
-
+//console.log(req.headers['content-type'])
+//console.log(req.rawHeaders)
 
 //console.log(url)
+
+const chunks = []; 
+for await ( const chunk of req){ 
+    chunks.push(chunk); 
+}
+console.log(Buffer.concat(chunks)); 
+const body = Buffer.concat(chunks).toString('utf-8');
+console.log(body); 
+console.log( JSON.parse(body));
+console.log(JSON.parse(body).username); 
+console.log(JSON.parse(body).senha);  
+
+
 
   
      //responstas de erro e status code
